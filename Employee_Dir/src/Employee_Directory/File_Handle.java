b0b363476c69516f9465fc8f10a624d96b504635
Arvind Hariharan Nair <arvindhn602@gmail.com>
2016-09-16 19:13:29 +0530

    add File Handling

--- Employee_Dir/src/Employee_Directory/File_Handle.java
+++ Employee_Dir/src/Employee_Directory/File_Handle.java
@@ -1,37 +1,1 @@
-package Employee_Directory;
-
-import java.io.File;
-import java.util.Formatter;
-import java.util.Scanner;
-
-/**
- * Created by arvindhn602 on 15/09/16.
- */
-public class File_Handle {
-    void File() {
-        int optn;
-        File file = new File("Employee_Details.txt");
-        Scanner sc = new Scanner(System.in);
-        if (file.exists()) {
-            System.out.println("File Exists");
-        }
-        else {
-            System.out.println("File Does not exist");
-            System.out.println("Press 1 to create a File");
-            optn = sc.nextInt();
-            if (optn == 1) {
-                File_create();
-            }
-        }
-    }
-
-    void File_create() {
-        try {
-            Formatter f = new Formatter("Employee_Details.txt");
-            System.out.println("File-Created");
-            File();
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
-    }
-}
+package Employee_Directory;import java.io.File;import java.util.Formatter;import java.util.Scanner;/** * Created by arvindhn602 on 15/09/16. */public class File_Handle {    void File() {        int optn;        File file = new File("Employee_Details.txt");        Scanner sc = new Scanner(System.in);        if (file.exists()) {            System.out.println("File Exists");        }        else {            System.out.println("File Does not exist");            System.out.println("Press 1 to create a File");            optn = sc.nextInt();            if (optn == 1) {                File_create();            }        }    }    void File_create() {        try {            Formatter f = new Formatter("Employee_Details.txt");            System.out.println("File-Created");            File();        } catch (Exception e) {            e.printStackTrace();        }    }}
